a3432f109507d72d7e13e5c2d6dce96994882151
philipp <philipp>
2023-12-17 11:02:17 -0300

    End of section 2

---
+++ Domain/Activity.cs
@@ -0,0 +1,14 @@
+namespace Domain
+{
+    public class Activity
+    {
+        //entity framework precisa que as propreidades sejam publicas
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+    }
+}
